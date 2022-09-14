3c1545ce3921c4528378d41376d5c320dc6151f7
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-14 19:38:24 +0600

    initial commit funtion practiced

---
+++ function.js
@@ -0,0 +1,11 @@
+function num(){
+    console.log('gjhgjh');
+    console.log('ami function practice kori');
+}
+
+num();
+
+function money(){
+    console.log("money can't buy happiness");
+}
+money();
