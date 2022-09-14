48da55a0d0b9bcfdc44aae08fd293a7cbb2c53de
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-14 21:15:47 +0600

    more funtion with parameter practiced

---
+++ add.js
@@ -0,0 +1,9 @@
+function add(num1, num2){
+    const sum = num1 + num2;
+    return sum;
+}
+
+const result1 = add(12, 23);
+const result2 = add(45, 23);
+const finalResult = add(result1, result2);
+console.log(finalResult);
