a9191e3e0421bde44e9a18ca694045f6b8bec003
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-14 21:29:24 +0600

    object in js practiced

---
+++ object.js
@@ -0,0 +1,15 @@
+var student = {id: 112,
+     name: 'rahmat',
+    class: 9,
+    marks: 78
+}
+
+var myComputer = {
+    brand: 'Lenovo',
+    price: 30000,
+    color: 'black',
+    processor: '23'
+}
+
+console.log(myComputer.color);
+console.log(student.name);
